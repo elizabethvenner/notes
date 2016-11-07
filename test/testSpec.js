4ddac964f16372b9f14ffc235d15ae06eca08437
James Gardiner <jamesgardiner075@gmail.com>
2016-11-07 16:56:00 +0000

    Add test for adding note

--- test/testSpec.js
+++ test/testSpec.js
@@ -1,1 +1,18 @@
-//test
+// insert text, click button, check array length
+
+function checkNoteHasBeenAdded() {
+    var notes = new Notes();
+    assert.isTrue(notes.items.length == 1);
+}
+
+var assert = {
+  isTrue: function(assertionToCheck) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+    }
+  }
+};
+
+function Notes() {
+  this.items = [];
+}
