d6f29ca16594c43ccf7dbaef76f1b430f914ddc7
James Gardiner <jamesgardiner075@gmail.com>
2016-11-07 18:11:01 +0000

    Create SpecRunner file

--- test/notesSpec.js
+++ test/notesSpec.js
@@ -1,14 +1,7 @@
-var assert = require("./test_helpers/assert");
+// var assert = require("./test_helpers/assert");
 
 function checkNoteHasBeenAdded() {
     var notes = new Notes();
     assert.isTrue(notes.items.length === 1);
     assert.isEqual(2, 2);
-}
-
-checkNoteHasBeenAdded();
-
-
-function Notes() {
-  this.items = ["sure"];
 }
