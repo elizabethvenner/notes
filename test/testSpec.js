e5e0ffbb8cdec86bf96b9b8c8c5da46a4414adf8
elizabethvenner <elizabethvenner@googlemail.com>
2016-11-08 13:24:34 +0000

    start adding test for clicking on link

--- test/testSpec.js
+++ test/testSpec.js
@@ -1,1 +1,8 @@
-//test
+'use strict';
+
+function testLink () {
+  document.getElementById("abbr").click();
+  if (document.getElementById("full") !== "banana") {
+    throw new Error("That's not a banana!");
+  }
+}
