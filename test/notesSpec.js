a166d45ff89d154ec925e843e1e7fd87b8645244
elizabethvenner <elizabethvenner@googlemail.com>
2016-11-09 10:45:40 +0000

    Add formatting to notesSpec and remove redundant function from index.html

--- test/notesSpec.js
+++ test/notesSpec.js
@@ -5,7 +5,7 @@ var checkNoteHasBeenAdded = function(){
   var notes = new Notes();
   try {
     assert.isTrue(notes.items.length === 1);
-    return "Passed - checkNoteHasBeenAdded "
+    return "Passed - checkNoteHasBeenAdded";
   }
   catch(error){
     return error;
@@ -14,15 +14,15 @@ var checkNoteHasBeenAdded = function(){
 
 var checkNoteHasOnly20Characters = function () {
   var notes = new Notes();
-  notes.items = ['1234567891011121314151617']
+  notes.items = ['1234567891011121314151617'];
   try {
     assert.isTrue(truncator(notes.items[0]).length < 21);
-    return "Passed - note has fewer than 20 characters"
+    return "Passed - note has fewer than 20 characters";
   }
   catch(error){
     return error;
-  };
-}
+  }
+};
 
 // ---Update this array to run tests!! ---
 var tests = [ checkNoteHasBeenAdded(), checkNoteHasOnly20Characters() ];
