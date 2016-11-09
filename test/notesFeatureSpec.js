dc72c401783c8a5832e524577e37f51c8203aa2e
James Gardiner <jamesgardiner075@gmail.com>
2016-11-09 16:11:54 +0000

    Extract feature tests

--- test/notesFeatureSpec.js
+++ test/notesFeatureSpec.js
@@ -1,27 +1,33 @@
-function testLink () {
-  document.getElementById(0).click();
-  function test() {
-    if (document.getElementById("full").innerHTML !== 'hellohellohellohello!!!') {
-      throw new Error("This is not the full note");
+(function(){
+  function testLink () {
+    document.getElementById(0).click();
+    function test() {
+      if (document.getElementById("full").innerHTML !== 'GET UP VERY VERY VERY VERY VERY EARLY!') {
+        throw new Error("This is not the full note");
+      }
+    }
+    setTimeout(test, 10);
+  }
+
+  function testTruncator (notes) {
+    if (document.getElementById(0).text.length < 23) {
+      throw new Error("Note not abbreviated");
     }
   }
-  setTimeout(test, 10);
-}
 
-function testTruncator (notes) {
-  renderNoteList(notes);
-  if (document.getElementById(0).text.length < 23) {
-    throw new Error("Note not abbreviated");
-  }
-}
 
-function testCreateNote () {
-  document.getElementById("note-content").value = "GET UP EARLIER!";
-  document.getElementById("add-note").click();
-  if (mynotes.items.length !== 2) {
-    throw new Error("Note array length has not increased");
+  function testCreateNote () {
+    document.getElementById("note-content").value = "GET UP VERY VERY VERY VERY VERY EARLY!";
+    document.getElementById("add-note").click();
+    if (mynotes.items.length !== 1) {
+      throw new Error("Note array length has not increased");
+    }
+    if (mynotes.items[0] != "GET UP VERY VERY VERY VERY VERY EARLY!") {
+      throw new Error("Text was not found in array");
+    }
   }
-  if (mynotes.items[1] != "GET UP EARLIER!") {
-    throw new Error("Text was not found in array");
-  }
-}
+
+  testCreateNote();
+  testLink();
+  testTruncator(notes);
+})();
