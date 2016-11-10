6a6155b46f0fbdf65384b1649f351ae0ce2a6256
elizabethvenner <elizabethvenner@googlemail.com>
2016-11-10 15:01:33 +0000

    refactor html element creation into testView.js and also split spec file into two separate test files

--- test/notesSpec.js
+++ test/notesSpec.js
@@ -1,6 +1,6 @@
 // var assert = require("./test_helpers/assert");
 console.log("hello1");
-var tests = [
+var notesTests = [
   (function checkDefaultIs0 (){
     var notes = new Notes();
     try {
@@ -49,53 +49,5 @@ var tests = [
   catch(error){
     return error;
   }
-})(),
-
-(function checkNoteHasOnly20Characters () {
-  var note = new Note('hellohellohellohello!!!');
-  try {
-    assert.isTrue(note.truncator(note.getNoteText()).length < 24);
-    return "Passed - note has fewer than 20 characters";
-  }
-  catch(error){
-    return error;
-  }
-})(),
-
-(function checkNoteText () {
-  var note = new Note('hellohellohellohello!!!');
-  try {
-    assert.isEqual(note.getNoteText(), 'hellohellohellohello!!!');
-    return "Passed - checkNoteText";
-  }
-  catch(error){
-    return error;
-  }
-})(),
-
-(function checkNote() {
-  var note = new Note('hellohellohellohello!!!');
-  try {
-    assert.belongsToPrototype(note, 'Note');
-    return "Passed - checkNoteText is prototype of Note";
-  }
-  catch(error){
-    return error;
-  }
-})()];
-
-
-// --- UPDATES THE SpecRunner HTML ---
-
-var runTests = function() {
-  tests.forEach(function(test){
-    div = document.getElementById("tests");
-    var li = document.createElement("li");
-    var text = document.createTextNode(test);
-    li.appendChild(text);
-    div.appendChild(li);
-  });
-  var total = "Total number of tests: " + tests.length;
-  var length = document.createTextNode(total);
-  document.getElementById("total").appendChild(length);
-};
+})()
+];
