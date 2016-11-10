b597a6a8160b8addcc8f52ae278043d024eb530b
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-10 11:23:07 +0000

    Create errors div

--- view.js
+++ view.js
@@ -1,6 +1,17 @@
 window.onload = function() {
+  heading();
+  errors();
+};
+
+function heading () {
   var h1 = document.createElement("H1");
   var t = document.createTextNode("My note");
   h1.appendChild(t);
   document.body.appendChild(h1);
+};
+
+function errors () {
+  var div = document.createElement('DIV');
+  div.setAttribute("id", "errors");
+  document.body.appendChild(div);
 };
